1038cc69c5ba52eadd1b9f08d85e0722243e83c2
CongCaoIT <103946367+CongCaoIT@users.noreply.github.com>
2025-02-17 18:26:41 +0700

    Thiet ke giao dien login quan tri

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,13 @@
 import config from "~/config";
 import UserLayout from "~/layouts/UserLayout";
+import Dashboard from "~/pages/Admin/Dashboard";
+import LoginAdmin from "~/pages/Admin/Login";
 import Home from "~/pages/Home";
 
 const publicRoutes = [
     { path: config.routes.home, component: Home, layout: UserLayout },
+    { path: config.routes.loginAdmin, component: LoginAdmin },
+    { path: config.routes.dashboard, component: Dashboard },
 ];
 const privateRoutes = [];
 
